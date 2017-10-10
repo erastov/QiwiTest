e8f3bf34183e79eab4bfd580097fbee28073660f
Erastov Fedor <fedor_erastov@mail.ru>
2017-10-10 16:14:42 +0300

    Remake serializers

--- orders/orders_app/serializers.py
+++ orders/orders_app/serializers.py
@@ -1,13 +1,8 @@
-from orders.orders_app.models import University, Student
+from orders.orders_app.models import Order
 from rest_framework import serializers
 
 
-class UniversitySerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = University
-        fields = '__all__'
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
+        model = Order
         fields = '__all__'
