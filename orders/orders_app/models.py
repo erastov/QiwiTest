3d6df47f62e6f09e8669ca4a853e144143c303f7
Erastov Fedor <fedor_erastov@mail.ru>
2017-10-10 16:15:14 +0300

    Add new model, remove old

--- orders/orders_app/models.py
+++ orders/orders_app/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 
-class University(models.Model):
-    name = models.CharField(max_length=50)
+
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
-        verbose_name = "University"
-        verbose_name_plural = "Universities"
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
 
     def __str__(self):
-        return self.name
-
-class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    university = models.ForeignKey(University)
-
-    class Meta:
-        verbose_name = "Student"
-        verbose_name_plural = "Students"
-
-    def __str__(self):
-        return '%s %s' % (self.first_name, self.last_name)
+        return '%s %s' % (self.name, self.price)
